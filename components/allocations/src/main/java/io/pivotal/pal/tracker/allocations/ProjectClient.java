a63872460dd2854506cd2ffc76ce8e2ec0b3888c
mrumpf <michael@rumpfonline.de>
2017-12-04 15:52:51 -0700

    Added Circuit Breaker support

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,18 +1,34 @@
 package io.pivotal.pal.tracker.allocations;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
     private final String registrationServerEndpoint;
+    private Map<Long, ProjectInfo> cache = new HashMap<>();
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations= restOperations;
         this.registrationServerEndpoint = registrationServerEndpoint;
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo pi = restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        cache.put(projectId, pi);
+        return pi;
+}
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        ProjectInfo pi = cache.get(projectId);
+        if (pi == null) {
+            pi = new ProjectInfo(false);
+        }
+        return pi;
     }
 }
--- components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
+++ components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
@@ -1,18 +1,34 @@
 package io.pivotal.pal.tracker.backlog;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
     private final String endpoint;
+    private Map<Long, ProjectInfo> cache = new HashMap<>();
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations = restOperations;
         this.endpoint = registrationServerEndpoint;
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo pi = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        cache.put(projectId, pi);
+        return pi;
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        ProjectInfo pi = cache.get(projectId);
+        if (pi == null) {
+            pi = new ProjectInfo(false);
+        }
+        return pi;
     }
 }
