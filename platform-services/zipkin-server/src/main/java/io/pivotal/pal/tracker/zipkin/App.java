d1b3bcb86f9302ffa2981aaf46dcf678bc6f9c8c
mrumpf <michael@rumpfonline.de>
2017-12-05 16:34:19 -0700

    Fixed annotation of the Zipkin server

--- platform-services/zipkin-server/src/main/java/io/pivotal/pal/tracker/zipkin/App.java
+++ platform-services/zipkin-server/src/main/java/io/pivotal/pal/tracker/zipkin/App.java
@@ -1,11 +1,12 @@
 package main.java.io.pivotal.pal.tracker.zipkin;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableZipkinStreamServer
-@ComponentScan("io.pivotal.pal.tracker.zipkin")
+@SpringBootApplication
 public class App {
         public static void main(String[] args) {
             SpringApplication.run(App.class, args);
